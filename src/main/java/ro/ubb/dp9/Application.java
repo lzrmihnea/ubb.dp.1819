25616d8184d5da87f85551cd4da29cbdb2768f84
Mihnea <lzr_mihnea@yahoo.com>
2019-02-25 22:44:29 +0200

    [NoRef-0] Initial commit with spring.io starter files

---
+++ src/main/java/ro/ubb/dp9/Application.java
@@ -0,0 +1,13 @@
+package ro.ubb.dp9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
