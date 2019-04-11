5bf8a6950656a2334c9eafa849acd48591b941fc
bogdan.mariesan <bogdan.mariesan@accesa.eu>
2019-04-11 14:00:16 +0300

    Lab4 exercises and lab doc.

---
+++ src/main/java/ro/ubb/dp1819/lab4/exercises/state/BillableDay.java
@@ -0,0 +1,12 @@
+package ro.ubb.dp1819.lab4.exercises.state;
+
+
+import ro.ubb.dp1819.lab4.exercises.Employee;
+
+public interface BillableDay {
+
+    boolean isEmployeeWorking(Employee employee);
+
+    boolean isEmployeeInOffice(Employee employee);
+
+}
